2971fd6f64a892b3924e796d9ce16441c7505739
Arturo Morales <alvartur34@outlook.com>
2021-04-03 15:47:58 -0600

    Version 0.0.4

--- js/my_scripts.js
+++ js/my_scripts.js
@@ -4,3 +4,34 @@
 let userLang = navigator.userLanguage || navigator.language;
 let lang = userLang.split("-", 1);
 document.getElementById("userLang").innerHTML = lang;
+
+//?-----------------------//
+//?---- Smooth Scroll ----//
+//?-----------------------//
+$('a[href*="#"]').on("click", function (e) {
+	e.preventDefault();
+
+	$("html, body").animate(
+		{
+			scrollTop: $($(this).attr("href")).offset().top
+		},
+		500,
+		"linear"
+	);
+});
+
+//?----------------//
+//?---- Loader ----//
+//?----------------//
+const progressBar = document.querySelector(".bar");
+document.ready = () => {
+	setTimeout(() => {
+		progressBar.classList.add("active");
+	}, 000); //!-->> ARREGLAR 1000
+};
+window.onload = () => {
+	progressBar.style.width = "100%";
+	setTimeout(() => {
+		document.getElementById("pageLoader").classList.add("hidde");
+	}, 00); //!-->> ARREGLAR 1500
+};
